3792fff247e2daacc837d9fca1e4b96e748a1344
m1tex <galov.kirya@gmail.com>
2026-06-24 07:17:50 +0300

    Add files via upload

---
+++ stroyKontrol.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict ETjApzM7AWHIxRZftndk3bgt3e4EVAxrTh8fVRDLzoi3t5CooqfbB3KpeKHGsia
+
+-- Dumped from database version 18.4
+-- Dumped by pg_dump version 18.4
+
+-- Started on 2026-06-24 07:15:36
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 6 (class 2615 OID 16594)
+-- Name: ais_stroykontrol; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA ais_stroykontrol;
+
+
+ALTER SCHEMA ais_stroykontrol OWNER TO postgres;
+
+-- Completed on 2026-06-24 07:15:36
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict ETjApzM7AWHIxRZftndk3bgt3e4EVAxrTh8fVRDLzoi3t5CooqfbB3KpeKHGsia
+
